1f1f84e429e0f2e103d3022f903a3c0f245e757b
Malcolm <malcolmf38@gmail.com>
2021-10-30 21:30:57 -0500

    Added redirect once article is saved

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,4 +9,16 @@ class ArticlesController < ApplicationController
     @title = article.title
     @description = article.description
   end
+
+  def new
+
+    
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    #render plain: @article.inspect
+    @article.save
+    redirect_to @article 
+  end
 end
