662d9b2019a27de8946e5cffa736965e634a1502
Malcolm <malcolmf38@gmail.com>
2021-11-03 22:05:29 -0500

    I've added basic flash validations, and the edit and update routes, views, and controllers

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,14 +11,25 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
+  end
 
+  def edit
     
   end
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
     #render plain: @article.inspect
-    @article.save
+    if @article.save
+      flash[:notice] = "You're article was saved successfully"
     redirect_to @article 
+    else 
+      render 'new'
+    end 
+  end
+
+  def update
+    
   end
 end
